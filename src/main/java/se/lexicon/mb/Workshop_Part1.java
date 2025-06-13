a30b64e89137b3655eb81e1afd8dab12eb44e0b4
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-06-13 20:26:29 +0300

    extracted applyOperator() and getAnswer() methods

--- src/main/java/se/lexicon/mb/Workshop_Part1.java
+++ src/main/java/se/lexicon/mb/Workshop_Part1.java
@@ -13,6 +13,9 @@ public class Workshop_Part1 {
 
     public static final List<String> VALID_OPERATORS = List.of("+", "-", "*", "/", "c", "e", "=");
     static Scanner scanner = new Scanner(System.in);
+    static String operator = "";
+    static int number = 0;
+    static int result = 0;
 
     public static void main(String[] args) {
         Calculator();
@@ -20,17 +23,13 @@ public class Workshop_Part1 {
 
     public static void Calculator() {
         System.out.println("Welcome! This calculator will help you perform basic math operations.");
-        String operator = "";
-        int number = 0;
-        int result = 0;
 
-        String expression = "";
 //        System.out.println("\n Enter number");
 //        number = scanner.nextInt();
         number = getIntInput("\n Enter number");
         result = number;
 
-        while(!operator.equals ("=")) {
+        while (!operator.equals("=")) {
 
             System.out.println("Enter + - * / or =, also you can enter c to clean and e to stop the program");
             operator = scanner.next();
@@ -38,7 +37,7 @@ public class Workshop_Part1 {
             if (operator.equalsIgnoreCase("E")) {
                 System.out.println("Goodbye!");
                 break;
-         } else if (operator.equalsIgnoreCase("C")) {
+            } else if (operator.equalsIgnoreCase("C")) {
                 System.out.println("All values have been reset");
                 number = 0;
                 operator = "";
@@ -50,27 +49,15 @@ public class Workshop_Part1 {
             } else if (operator.equals("=")) {
                 System.out.println("Result:" + result);
 
-                while(true) {
-                    System.out.println("Continue? (y/n):");
-                    String answer = scanner.next();
-                    if (answer.equalsIgnoreCase("y")) {
-                        System.out.println("Enter number:");
-                        number = scanner.nextInt();
-                        result = number;
-                        operator = "";
-                        break;
-                    } else if (answer.equalsIgnoreCase("n")) {
-                        System.out.println("Goodbye!");
-                        return;
-                    } else {
-                        System.out.println("Invalid answer. Try again");
+                while (true) {
 
-                    }
+                    if (getAnswer()){
+                        break;
+                    };
                 }
                 continue;
 
-            }
-            else if (!VALID_OPERATORS.contains(operator.toLowerCase())) {
+            } else if (!VALID_OPERATORS.contains(operator.toLowerCase())) {
 
                 System.out.print("Operator doesn't correct. Try again!");
                 continue;
@@ -78,42 +65,67 @@ public class Workshop_Part1 {
 //            System.out.println("Enter next number:");
 //            int nextNumber = scanner.nextInt();
             int nextNumber = getIntInput("Enter next number:");
-            switch (operator){
-                case "+":
-                    result = result + nextNumber;
-                    break;
-                case "-":
-                    result = result - nextNumber;
-                    break;
-                case "*":
-                    result = result * nextNumber;
-                    break;
-                case "/":
-                    if (nextNumber != 0){
-                        result = result / nextNumber;
-                    } else {
-                        System.out.println("Cannot divide by zero! Try again!");
-                    }
-                    break;
-            }
+
+            result = applyOperator(result, operator, nextNumber);
             System.out.println("Current result: " + result);
         }
 
     }
+
     public static int getIntInput(String message) {
-        while (true){
-        System.out.println(message);
-        String input = scanner.next();
-        if (input.equalsIgnoreCase("e")) {
+        while (true) {
+            System.out.println(message);
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("e")) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            } else {
+                try {
+                    return Integer.parseInt(input);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid number. Try again");
+                }
+            }
+        }
+    }
+
+    public static int applyOperator(int result, String operator, int nextNumber) {
+        switch (operator) {
+            case "+":
+                result = result + nextNumber;
+                break;
+            case "-":
+                result = result - nextNumber;
+                break;
+            case "*":
+                result = result * nextNumber;
+                break;
+            case "/":
+                if (nextNumber != 0) {
+                    result = result / nextNumber;
+                } else {
+                    System.out.println("Cannot divide by zero! Try again!");
+                }
+                break;
+        }
+        return result;
+    }
+
+    public static boolean getAnswer() {
+        System.out.println("Continue? (y/n):");
+        String answer = scanner.next();
+        if (answer.equalsIgnoreCase("y")) {
+            System.out.println("Enter number:");
+            number = scanner.nextInt();
+            result = number;
+            operator = "";
+            return true;
+        } else if (answer.equalsIgnoreCase("n")) {
             System.out.println("Goodbye!");
             System.exit(0);
         } else {
-            try {
-                return Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid number. Try again");
-            }
+            System.out.println("Invalid answer. Try again");
         }
-        }
+        return false;
     }
 }
